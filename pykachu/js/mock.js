a5953d6fe2f5cd9720a08e30eaf0c021cce0d332
Jo�o Leite <joao.leite@sieve.com.br>
2013-11-01 04:49:50 -0200

    kray
    Filtro boladão funcionando.
    
    Issssssssssss wi

--- pykachu/js/mock.js
+++ pykachu/js/mock.js
@@ -10,14 +10,14 @@ mockProcess = function(n) {
                 cliente: 'FIVE IMPORTS COM. IMP &amp; EXP. LTDA - ME - Buscapé',
                 cliente_id: '1135',
                 solicitante: 'Joao Leite',
-                // Campos Calculados pela Tela
-                velocidade: '10/s',
-                termina_em: '39 min',
+                // Campos Calculados pela Node
+                // velocidade: Math.floor((Math.random()*10)+1),
+                // termina_em: '39 min',
                 // Campos obrigatorios pelo app.js
+                // data_inicio: '10/10/2013 às 15:43:21',
+                // data_fim: '10/10/2013 às 15:43:21',
                 id: 162532134165234,
                 titulo: 'Criar tag em grupo de tags ja existentii',
-                data_inicio: '10/10/2013 às 15:43:21',
-                data_fim: '10/10/2013 às 15:43:21',
                 passo_msg: 'Preparando Backup',
                 passo_atual: Math.floor((Math.random()*49)+1),
                 passo_total: Math.floor((Math.random()*100)+50),
