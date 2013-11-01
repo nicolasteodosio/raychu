da6703b180b99ece1b4f5ed9894ea28313f5fa87
victornovais <victorh.novaisr@gmail.com>
2013-10-31 22:34:17 -0200

    Refatorando variável e algumas lógicas.

--- app.js
+++ app.js
@@ -33,60 +33,65 @@ app.post('/process', function(request, response) {
 
     json.id = processId;
 
-    console.log(json);
     process[processId] = json;
     response.send(json);
 });
 
 // Função a ser chamada quando um processo passa para o próximo passo, se houver mais de um
-app.post(/^\/process\/(\d+)\/next$/, function(req, res){
+app.post(/^\/process\/(\d+)\/next$/, function(request, response){
     console.log("Proximo Processo");
-    var id = req.params[0];
-    process[Pykachu_id]['step']++;
 
-    msg_step = req.body.msg_step;
+    var id = request.params[0];
 
-    if (!(msg_step === undefined))
-        process[Pykachu_id]['msg_step'] = msg_step;
+    process[id]['step']++;
+    msg_step = request.body.msg_step;
 
-    res.send(process[id]);
+    if (msg_step !== undefined) {
+        process[id]['msg_step'] = msg_step;
+    }
+
+    response.send(process[id]);
 });
 
 // Função a ser chamada para receber parâmetros de um processo
-app.get(/^\/process\/(\d+)$/, function(req, res){
-    console.log("Devolvendo processo");
-    var id = req.params[0];
-    res.send(process[id]);
+app.get(/^\/process\/(\d+)$/, function(request, response){
+    console.log("Retornando processo");
+
+    var id = request.params[0];
+    response.send(process[id]);
 });
 
 // Função a ser chamada quando um processo tiver algum parâmetro mudado
-app.put(/^\/process\/(\d+)$/, function(req, res){
+app.put(/^\/process\/(\d+)$/, function(request, response){
     console.log("Atualizando Processo");
-    var id = req.params[0];
 
-    step = req.body.step;
-    total = req.body.total;
-    msg_step = req.body.msg_step;
-    status = req.body.status;
+    var id = request.params[0],
+        step = request.body.step,
+        total = request.body.total,
+        msg_step = request.body.msg_step,
+        status = request.body.status;
 
-    if (step !== undefined)
+    if(step !== undefined)
         process[id]['step'] = parseInt(step);
-    if (total !== undefined)
+    if(total !== undefined)
         process[id]['total'] = parseInt(total);
-    if (msg_step !== undefined)
+    if(msg_step !== undefined)
         process[id]['msg_step'] = msg_step;
-    if (status !== undefined)
+    if(status !== undefined)
         process[id]['status'] = parseInt(status);
 
-    res.send(process[id]);
+    response.send(process[id]);
 });
 
 // Função a ser chamada quando algum processo é removido
-app.delete(/^\/process\/(\d+)$/, function(req, res){
+app.delete(/^\/process\/(\d+)$/, function(request, response){
     console.log("Deletando processo");
-    var id = req.params[0];
+
+    var id = request.params[0];
+
     delete process[id];
-    res.send(process[id]);
+
+    response.send(process[id]);
 });
 
 
